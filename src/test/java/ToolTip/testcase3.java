ee2a035be3d472c56e1f35a1423aafa6d1ae9d2c
testlead <gptestlead@gmail.com>
2021-01-02 21:05:35 -0600

    Changes done

---
+++ src/test/java/ToolTip/testcase3.java
@@ -0,0 +1,10 @@
+package ToolTip;
+
+public class testcase3 {
+
+	public static void main(String[] args) {
+		System.out.println("From Github to Workstation");
+		System.out.println("Modify from Workstation");
+	}
+
+}
---
+++ src/test/java/ToolTip/testcase4.java
@@ -0,0 +1,9 @@
+package ToolTip;
+
+public class testcase4 {
+
+	public static void main(String[] args) {
+		System.out.println("From worksstation to Github");
+	}
+
+}
